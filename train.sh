9dfebf28adff67c7323fec2f49bd2e0d34bfa3f0
bob wang <xingbo.wang@winndoo.com>
2020-12-15 17:09:05 +0800

    batch size

--- train.sh
+++ train.sh
@@ -11,7 +11,7 @@ python bert/run_classifier.py \
 --bert_config_file=$BERT_BASE_DIR/bert_config.json \
 --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
 --max_seq_length=512 \
---train_batch_size=8 \
+--train_batch_size=4 \
 --learning_rate=2e-5 \
 --num_train_epochs=3.0 \
 --output_dir=./mytask_output
